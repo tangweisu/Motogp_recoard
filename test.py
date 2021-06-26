6d30c5f1959f108b01111358ef80ec39af071a3d
Phillace001 <44096411+tangweisu@users.noreply.github.com>
2021-06-27 01:36:03 +0800

    Add files via upload
    first commit

---
+++ test.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import bs4
+
+url = 'https://datamotogp.azurewebsites.net/GpResults/Index/64'
+r = requests.get(url)
+web_content = r.text
+soup = BeautifulSoup(web_content, 'lxml')
+
+title = soup.find('div' ,class_=["col-md-8"]).tbody.find_all('tr')
+Rider = [t.find('a').text for t in title]
+
+lapData = []
+for row in title:
+        rid = row.find_all('td')[5].text
+        rid = float(rid)
+        min = str(int(rid/60.0))
+        sec = str(int(rid-60.0))
+        rid = min+'.'+sec
+        lapData.append(rid)
+print('2021 Germen GP')
+df = pd.DataFrame(
+{
+    'Riders': Rider,
+    'Best5' : lapData
+})
+df.index=df.index+1
+
+print(df)
